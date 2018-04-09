706a0b7892fb600c447ff0bc0d03e2b1fde597b5
aefiq96 <g00324934@gmit.ie>
2018-04-09 23:52:43 +0100

    first commit

---
+++ WeatherApp/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace WeatherApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lbl_cityName = new System.Windows.Forms.Label();
+            this.lbl_countryName = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lbl_Temp = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbl_cityName
+            // 
+            this.lbl_cityName.AutoSize = true;
+            this.lbl_cityName.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_cityName.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.lbl_cityName.Location = new System.Drawing.Point(28, 24);
+            this.lbl_cityName.Name = "lbl_cityName";
+            this.lbl_cityName.Size = new System.Drawing.Size(159, 46);
+            this.lbl_cityName.TabIndex = 0;
+            this.lbl_cityName.Text = "Galway";
+            //this.lbl_cityName.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lbl_countryName
+            // 
+            this.lbl_countryName.AutoSize = true;
+            this.lbl_countryName.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_countryName.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.lbl_countryName.Location = new System.Drawing.Point(36, 92);
+            this.lbl_countryName.Name = "lbl_countryName";
+            this.lbl_countryName.Size = new System.Drawing.Size(40, 31);
+            this.lbl_countryName.TabIndex = 2;
+            this.lbl_countryName.Text = "IE";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(42, 138);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 87);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lbl_Temp
+            // 
+            this.lbl_Temp.AutoSize = true;
+            this.lbl_Temp.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Temp.ForeColor = System.Drawing.Color.White;
+            this.lbl_Temp.Location = new System.Drawing.Point(148, 149);
+            this.lbl_Temp.Name = "lbl_Temp";
+            this.lbl_Temp.Size = new System.Drawing.Size(109, 76);
+            this.lbl_Temp.TabIndex = 4;
+            this.lbl_Temp.Text = "10";
+            //this.lbl_Temp.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(799, 477);
+            this.Controls.Add(this.lbl_Temp);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lbl_countryName);
+            this.Controls.Add(this.lbl_cityName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_cityName;
+        private System.Windows.Forms.Label lbl_countryName;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lbl_Temp;
+    }
+}
+
---
+++ WeatherApp/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace WeatherApp
+{
+    public partial class Form1 : Form
+    {
+        const string APPID = "";
+        string cityName = "Colombo";
+        //private object lbl_cityName;
+        //private object lbl_countryName;
+        //private object lbl_Temp;
+
+        public Form1()
+        {
+            InitializeComponent();
+            getWeather();
+        }
+
+        /*private void label1_Click(object sender, EventArgs e)
+        {
+
+        }*/
+
+        void getWeather()
+        {
+            using (WebClient web = new WebClient())
+            {
+                //where do i get this url?
+                string url = string.Format("http://api.openweathermap.org/data/2.5/weather?q=Galway&appid=542ffd081e67f4512b705f89d2a611b2&units=metric&cnt=6");
+
+                var json = web.DownloadString(url);
+
+                var result = JsonConvert.DeserializeObject<weatherInfo.Root>(json);
+
+                weatherInfo.Root outPut = result;
+
+                
+
+                lbl_cityName.Text = string.Format("{0}", outPut.name);
+                lbl_countryName.Text = string.Format("{0}", outPut.sys.country);
+                lbl_Temp.Text = string.Format("{0} \u00B0" + "C", outPut.main.temp);
+
+               
+
+            }
+
+            
+        }
+
+        /*private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }*/
+    }
+}
