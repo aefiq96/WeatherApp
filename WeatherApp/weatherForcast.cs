94403d8b7329c5d3e610bccd31eadad8990cbe63
aefiq96 <g00324934@gmit.ie>
2018-04-10 13:55:16 +0100

    list update

--- WeatherApp/weatherForcast.cs
+++ WeatherApp/weatherForcast.cs
@@ -15,6 +15,11 @@ namespace WeatherApp
         public string name { get; set; }
     }
     public class list {
+        public double dt { get; set; } //day in milli seconds
+        public double pressure { get; set; } //pressure hpa
+        public double humidity { get; set; }//humidity %
+        public double speed { get; set; }//wind speed km/h
+
 
     }
 }
