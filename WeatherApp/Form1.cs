ceb7f21b3ec454aae45c4eb5d653b80d241390c1
aefiq96 <g00324934@gmit.ie>
2018-04-11 22:54:27 +0100

    form updated

--- WeatherApp/Form1.cs
+++ WeatherApp/Form1.cs
@@ -66,11 +66,11 @@ namespace WeatherApp
                 lbl_wind_2.Text = string.Format("{0} km/h", forcast.list[1].speed); //weather temperature
 
                 //day after tomorrow
-                lbl_day_3.Text = string.Format("{0}", getDate(forcast.list[1].dt).DayOfWeek); //returning Day
+                lbl_day_3.Text = string.Format("{0}", getDate(forcast.list[2].dt).DayOfWeek); //returning Day
                 lbl_cond_3.Text = string.Format("{0}", forcast.list[1].weather[0].main); //weather condition
                 lbl_des_3.Text = string.Format("{0}", forcast.list[1].weather[0].description); //weather description
-                lbl_temp_3.Text = string.Format("{0} \u00B0" + "C", forcast.list[1].temp.day); //weather temperature
-                lbl_wind_3.Text = string.Format("{0} km/h", forcast.list[1].speed); //weather temperature
+                lbl_temp_3.Text = string.Format("{0} \u00B0" + "C", forcast.list[2].temp.day); //weather temperature
+                lbl_wind_3.Text = string.Format("{0} km/h", forcast.list[2].speed); //weather temperature
 
             }
         }
